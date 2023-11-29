84959065bcfcaf920b50a2b033cf6d6f8ef67bf9
Takeo Arai <takeo.60271.arai@gmail.com>
2023-11-30 07:08:08 +0900

    1 commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Layout from '../../components/Layout'
-const inter = Inter({ subsets: ['latin'] })
+import Layout from '../../components/Layout';
 
 export default function Home() {
   return (
